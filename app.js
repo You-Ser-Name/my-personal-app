f80569f132134dc3fa1193736eb030fa6ab372cc
You-Ser-Name <doug.aremka@gmail.com>
2026-08-23 22:05:14 -0500

    Delete app.js

--- app.js
+++
@@ -1,9 +0,0 @@
-if ("serviceWorker" in navigator) {
-    navigator.serviceWorker.register("./service-worker.js")
-        .then(() => {
-            console.log("Service worker registered");
-        })
-        .catch(error => {
-            console.error("Service worker registration failed:", error);
-        });
-}
